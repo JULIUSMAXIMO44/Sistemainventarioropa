8967cd14d0cb879a9dc080f5de273811f13c193d
JULIUSMAXIMO44 <jrudass@unicartagena.edu.co>
2026-08-08 11:33:26 -0500

    Change 'Casual' option to 'Informal' in ProductForm

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -65,7 +65,7 @@ const ProductForm = ({ onAdd }) => {
           <option value="Calzado">Calzado</option>
           <option value="Accesorios">Accesorios</option>
           <option value="Formal">Formal</option>
-          <option value="Casual">Casual</option>
+          <option value="Casual">Informal</option>
           <option value="Deportivo">Deportivo</option>
         </select>
         <input
